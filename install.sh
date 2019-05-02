66fc365fd003af1c3a4be70e93ac7742ec512c4d
Ahmed Mohamed <44830370+Ahmedmahmed8a@users.noreply.github.com>
2019-05-02 05:37:26 +0200

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/bash
+apt update
+apt upgrade
+apt install cowsay
+apt install python2
+pip2 install requests
+unzip -o fgroup.zip
+echo "rm -r $HOME/../usr/bin/fgroup">uninstall.sh
+chmod +x *
+mv token.pyc $HOME/../usr/bin/
+mv idgenerator.pyc $HOME/../usr/bin/
+mv fgroup $HOME/../usr/bin/
+cd $HOME/../usr/bin/
+chmod +x *
+echo -e "\e[0;32m"
+cowsay '
+Facebook group brute force installed
+ you can open it by typing fgroup
+'
